581df079b18a5a909cde9c86ace03e2ed1c8bd34
Tobin Dunn <tobin@thedunnhome.com>
2024-10-23 14:10:54 -0400

    Automated backup 2024-10-23 14:10:54

--- git_backup.sh
+++ git_backup.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 cd /config
 
+# Log the start of the backup
 echo "Starting Git backup" > /config/git_backup.log
 
-# Add all changes to Git
-/usr/bin/git add . >> /config/git_backup.log 2>&1
+# Configure Git user details
+git config user.name "Tobin Dunn"
+git config user.email "tobin@thedunnhome.com"
 
+# Add all changes to Git
+git add . >> /config/git_backup.log 2>&1
 
 # Commit the changes with a timestamp
-/usr/bin/git commit -m "Automated backup $(date +'%Y-%m-%d %H:%M:%S')" >> /config/git_backup.log 2>&1
+git commit -m "Automated backup $(date +'%Y-%m-%d %H:%M:%S')" >> /config/git_backup.log 2>&1
 
-# Configure the SSH key path
-git config core.sshCommand "ssh -i /config/.ssh/id_rsa -F /dev/null"
-
-# Push the changes to the master branch on GitHub
-/usr/bin/git push origin master --force >> /config/git_backup.log 2>&1
+# Force push the changes using HTTPS and personal access token
+git push https://dobytunn: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@github.com/dobytunn/Home-Assistant-Config.git --force >> /config/git_backup.log 2>&1
 
 echo "Backup completed" >> /config/git_backup.log
