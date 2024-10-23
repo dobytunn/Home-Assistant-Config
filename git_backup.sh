1d1b691d8262c91a9ac3af3a6d69e93a535464c8
root <root@homeassistant.local.hass.io>
2024-10-23 12:25:41 -0400

    Automated backup 2024-10-23 12:25:41

--- git_backup.sh
+++ git_backup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 cd /config
 
+echo "Starting Git backup" > /config/git_backup.log
+
 # Add all changes to Git
-git add .
+/usr/bin/git add . >> /config/git_backup.log 2>&1
+
 
 # Commit the changes with a timestamp
-git commit -m "Automated backup $(date +'%Y-%m-%d %H:%M:%S')"
+/usr/bin/git commit -m "Automated backup $(date +'%Y-%m-%d %H:%M:%S')" >> /config/git_backup.log 2>&1
 
 # Push the changes to the master branch on GitHub
-git push origin master
+/usr/bin/git push origin master >> /config/git_backup.log 2>&1
+
+echo "Backup completed" >> /config/git_backup.log
